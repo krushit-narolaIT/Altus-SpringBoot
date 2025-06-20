665dc3f5d04857f1319f84e4e85f2cb5792707fa
krushit-narolaIT <ksb@narola.email>
2025-06-20 18:34:06 +0530

    First Commit

---
+++ src/main/java/com/narola/AltusSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.narola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltusSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AltusSpringBootApplication.class, args);
+    }
+
+}
